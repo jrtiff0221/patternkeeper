75c29f89ebda93c051caf8a85179ea3c6703b846
johnnae <jrtiff0221@gmail.com>
2020-09-09 08:44:21 -0600

    wip

---
+++ app/controllers/patterns_controller.rb
@@ -0,0 +1,70 @@
+class PatternsController < ApplicationController
+  before_action :set_pattern, only: [:show, :edit, :update, :destory]
+
+def new
+  @pattern = Pattern.new
+end
+
+def create
+  
+
+  @pattern = current_user.patterns.build(pattern_params)
+  if @pattern.valid?
+    @pattern.save
+    
+    redirect_to pattern_path(@pattern)
+  else
+    render :new 
+  end
+end
+
+def index
+  if params[:category]
+    
+    @patterns = Pattern.search_by_category(params[:category])
+    @patterns = Pattern.order("created_at ASC") if @patterns == []
+
+  else
+   
+    @patterns = Pattern.order("created_at ASC")
+  end
+end
+
+def show 
+  @pattern = Pattern.find_by(id: params[:id])
+end 
+
+def edit
+  set_pattern
+end
+
+def update
+  set_pattern
+  if current_user.id == @pattern.user_id && @pattern.update(pattern_params)
+    redirect_to  pattern_path(@pattern)
+  else 
+    render :edit 
+  end
+end
+
+def destroy
+  set_pattern 
+  if  current_user.id == @pattern.user_id
+    @pattern.destroy 
+    redirect to patterns_path
+  end
+end
+private
+
+def set_pattern 
+  @pattern = Pattern.find_by(id: params[:id])
+  if !@pattern 
+    redirect_to patterns_path 
+  end
+end 
+
+def pattern_params 
+
+  params.require(:pattern).permit(:title, :author, :published_date, :category, :difficulty, :description, :name, :website, :user_id)
+end 
+end
---
+++ app/models/pattern.rb
@@ -0,0 +1,21 @@
+class Pattern < ApplicationRecord
+    validates :title, :author, :published_date, :category, :difficulty, :description, :name, :user_id, presence: true
+
+
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+    accepts_nested_attributes_for :comments
+
+    scope :crocheting, -> {where(category: "Crocheting")}
+    scope :knitting, -> {where(category: "Knitting")}
+    scope :macrame, -> {where(category: "Macrame")}
+    scope :sewing, -> {where(category: "Sewing")}
+
+    scope :easy, -> {where(difficulty: "1 Easy")}
+    scope :intermediate, -> {where(difficulty: "2 Intermediate")}
+    scope :advanced, -> {where(difficulty: "3 Advanced")}
+    
+    scope :search_by_category, ->(search_by_category){where("category=?")}
+    scope :ordered, -> {order('created_at ASC')}
+    
+end
